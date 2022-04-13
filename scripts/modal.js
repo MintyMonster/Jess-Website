359fcac3c1a9ac94240c472c1be90050fafc1253
MintyMonster <camerongliddell@gmail.com>
2022-04-13 03:11:39 +0100

    A few fixes here and there...

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,13 +1,14 @@
-function galleryOnClick(element){
+function galleryOnClick(element) {
   var modal = document.getElementById("modal");
   var imgSrc = $(element).find('img').attr('src');
   var modalImage = document.getElementById("modal-image");
+  var image = $(element).find('img');
 
   modal.style.display = "block";
   modalImage.src = imgSrc;
 }
 
-function modalClose(element){
+function modalClose(element) {
   var modal = document.getElementById("modal");
   modal.style.display = "none";
 }
